5468e4ff83b533e85742140e400b4408943dffa5
Haggets <70298382+Haggets@users.noreply.github.com>
2021-09-10 00:45:31 -0400

    Basic Layout, still WIP

--- TTKCalculator/Form1.Designer.cs
+++ TTKCalculator/Form1.Designer.cs
@@ -29,20 +29,99 @@ namespace TTKCalculator
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.damageLabel = new System.Windows.Forms.Label();
+            this.fireRateLabel = new System.Windows.Forms.Label();
+            this.magazineSizeLabel = new System.Windows.Forms.Label();
+            this.reloadTimeLabel = new System.Windows.Forms.Label();
+            this.damageValue = new System.Windows.Forms.NumericUpDown();
+            this.fireRateValue = new System.Windows.Forms.NumericUpDown();
+            this.magazineValue = new System.Windows.Forms.NumericUpDown();
+            this.reloadTimeValue = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.damageValue)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fireRateValue)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.magazineValue)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.reloadTimeValue)).BeginInit();
             this.SuspendLayout();
+            // 
+            // damageLabel
+            // 
+            resources.ApplyResources(this.damageLabel, "damageLabel");
+            this.damageLabel.Name = "damageLabel";
+            // 
+            // fireRateLabel
+            // 
+            resources.ApplyResources(this.fireRateLabel, "fireRateLabel");
+            this.fireRateLabel.Name = "fireRateLabel";
+            // 
+            // magazineSizeLabel
+            // 
+            resources.ApplyResources(this.magazineSizeLabel, "magazineSizeLabel");
+            this.magazineSizeLabel.Name = "magazineSizeLabel";
+            // 
+            // reloadTimeLabel
+            // 
+            resources.ApplyResources(this.reloadTimeLabel, "reloadTimeLabel");
+            this.reloadTimeLabel.Name = "reloadTimeLabel";
+            // 
+            // damageValue
+            // 
+            resources.ApplyResources(this.damageValue, "damageValue");
+            this.damageValue.Name = "damageValue";
+            this.damageValue.ValueChanged += new System.EventHandler(this.value_Update);
+            this.damageValue.Click += new System.EventHandler(this.damageValue_Click);
+            // 
+            // fireRateValue
+            // 
+            resources.ApplyResources(this.fireRateValue, "fireRateValue");
+            this.fireRateValue.Name = "fireRateValue";
+            this.fireRateValue.ValueChanged += new System.EventHandler(this.value_Update);
+            // 
+            // magazineValue
+            // 
+            resources.ApplyResources(this.magazineValue, "magazineValue");
+            this.magazineValue.Name = "magazineValue";
+            this.magazineValue.ValueChanged += new System.EventHandler(this.value_Update);
+            // 
+            // reloadTimeValue
+            // 
+            resources.ApplyResources(this.reloadTimeValue, "reloadTimeValue");
+            this.reloadTimeValue.Name = "reloadTimeValue";
+            this.reloadTimeValue.ValueChanged += new System.EventHandler(this.value_Update);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(790, 251);
+            this.Controls.Add(this.damageLabel);
+            this.Controls.Add(this.fireRateLabel);
+            this.Controls.Add(this.magazineSizeLabel);
+            this.Controls.Add(this.reloadTimeLabel);
+            this.Controls.Add(this.reloadTimeValue);
+            this.Controls.Add(this.magazineValue);
+            this.Controls.Add(this.fireRateValue);
+            this.Controls.Add(this.damageValue);
+            this.Cursor = System.Windows.Forms.Cursors.IBeam;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.damageValue)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fireRateValue)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.magazineValue)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.reloadTimeValue)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+        private System.Windows.Forms.Label damageLabel;
+        private System.Windows.Forms.Label magazineSizeLabel;
+        private System.Windows.Forms.Label reloadTimeLabel;
+        private System.Windows.Forms.NumericUpDown damageValue;
+        private System.Windows.Forms.Label fireRateLabel;
+        private System.Windows.Forms.NumericUpDown fireRateValue;
+        private System.Windows.Forms.NumericUpDown magazineValue;
+        private System.Windows.Forms.NumericUpDown reloadTimeValue;
     }
 }
 
--- TTKCalculator/Form1.cs
+++ TTKCalculator/Form1.cs
@@ -16,5 +16,61 @@ namespace TTKCalculator
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        // Text changes //
+        private void damageValue_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void value_Update(object sender, EventArgs e)
+        {
+            calculateTTK(damageValue.Value, fireRateValue.Value, magazineValue.Value, reloadTimeValue.Value);
+        }
+
+
+        // Algorithm that calculates the TTK and DPS based on the inputs.
+        private decimal calculateTTK(decimal damage, decimal fireRate, int magSize, decimal reloadSpeed, decimal enemyHealth  =  100)
+        {
+            if (damage < 1) damage = 1; // Preventing the possibility of an endless loop 
+            if (fireRate < 0) fireRate = 0;
+            if (magSize <= 0) magSize = 1; // Preventing jank
+            if (reloadSpeed < 0) reloadSpeed = 0;
+
+            int shotsTaken = 0;
+            int ammoInMag = magSize;
+            decimal timeTaken = 0;
+            decimal dps = CalculateDPS(damage, fireRate);
+
+            while (enemyHealth > 0)
+            {
+                enemyHealth -= damage;
+                shotsTaken++;
+                if (enemyHealth <= 0) break;
+                // timeTaken += fireRate;
+                ammoInMag--;
+
+                // Reload
+                if (ammoInMag <= 0)
+                {
+                    // timeTaken += reloadSpeed;
+                    ammoInMag = magSize;
+                }
+            }
+
+            return timeTaken;
+        }
+
+        private decimal CalculateDPS(decimal damage, decimal fireRate) => damage * fireRate;
     }
 }
