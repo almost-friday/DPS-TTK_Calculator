6ee0f4357e1d731dfd61ba3a33f47dca2e3e19b5
Haggets <70298382+Haggets@users.noreply.github.com>
2021-09-17 14:29:09 -0400

    Logic update

--- Calculator.js
+++ Calculator.js
@@ -1,11 +1,22 @@
-// Calculation
+// Helper functions
+let FireShot = magazine => magazine > 0 ? --magazine : false;
+
+function Reload(magazineSize, reloadSpeed, totalTime) { return (magazineSize); }
+
+// Main calculation
 function TimeToKill(damage, fireRate, magazineSize, reloadSpeed, health) {
-    let ttk = 0;
+    let timeToKill = 0;
     let currentMagCapacity = magazineSize;
-    console.log("Hello");
-    console.log(currentMagCapacity);
+
     currentMagCapacity = FireShot(currentMagCapacity);
     console.log(currentMagCapacity);
+    if (currentMagCapacity === 0) {
+        currentMagCapacity, timeToKill = Reload(magazineSize, reloadSpeed, timeToKill);
+        console.log(currentMagCapacity);
+        console.log(timeToKill);
+        console.log("Hellooo");
+    }
+
     /*
     let timer = 0;
     if (damage <= 0) {
@@ -16,32 +27,18 @@ function TimeToKill(damage, fireRate, magazineSize, reloadSpeed, health) {
     ttk = damage * fireRate;
     console.log(ttk);
     */
-}
-function FireShot(mag){
-    mag => mag > 0 ? --mag : false;
-    /*
-    if (mag > 0){
-        return --mag;
-    } 
-    else return false;
-    */
+
+    return timeToKill;
 }
 
-function Reload(currentmagcapacity, magcapacity, reloadspeed, totaltime){
-    currentmagcapacity = magcapacity;
-    totaltime += reloadspeed;
-}
-
-// Updates calculation
+// Calculation update
 function Update() {
-    console.log("Updated kindof");
-    let damage = document.getElementById("damageEntry");
-    let fireRate = document.getElementById("fireRateEntry");
-    let magazineSize = document.getElementById("magazineSizeEntry");
-    let reloadSpeed = document.getElementById("reloadSpeedEntry");
-    let enemyHealth = document.getElementById("healthEntry");
+    const [damage, fireRate, magazineSize, reloadSpeed, enemyHealth] = document.getElementsByTagName("input");
+    const timeToKill = document.getElementById("timeToKillResult");
+
     if (damage.value && fireRate.value && magazineSize.value && reloadSpeed.value && enemyHealth.value) {
         console.log("Updated!");
-        TimeToKill(damage.value, fireRate.value, magazineSize.value, reloadSpeed.value, enemyHealth.value);
+        totalTime = TimeToKill(damage.value, fireRate.value, magazineSize.value, reloadSpeed.value, enemyHealth.value);
+        timeToKill.value = totalTime;
     }
 }
