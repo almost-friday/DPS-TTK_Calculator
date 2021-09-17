274f9664ad406fb0cd3bc911154fa31ec81ccad8
Haggets <70298382+Haggets@users.noreply.github.com>
2021-09-17 00:25:08 -0400

    Code cleanup

--- Calculator.js
+++ Calculator.js
@@ -5,10 +5,9 @@ function timeToKill(damage, fireRate, magazineSize, reloadSpeed, health) {
 
 // Updates calculation
 function update() {
-    const element = document.getElementById
-    element("damageEntry").val
+    const element = document.getElementById;
     if (element("damageEntry").value && element("fireRateEntry").value && element("magazineSizeEntry").value && element("reloadSpeedEntry").value && element("healthEntry").value) {
-        console.log("Updated!")
-        // timeToKill(element("damageEntry", "fireRateEntry", "magazineSizeEntry", "reloadSpeedEntry", "healthEntry"))
+        console.log("Updated!");
+        // timeToKill(element("damageEntry"), element("fireRateEntry"), element("magazineSizeEntry"), element("reloadSpeedEntry"), element("healthEntry"));
     }
 }
