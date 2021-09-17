595d440751a9b02e1f83cc9e922ce9e7a01929c3
Haggets <70298382+Haggets@users.noreply.github.com>
2021-09-17 00:17:11 -0400

    Entry update and health entry

---
+++ Calculator.js
@@ -0,0 +1,14 @@
+// Calculation
+function timeToKill(damage, fireRate, magazineSize, reloadSpeed, health) {
+
+}
+
+// Updates calculation
+function update() {
+    const element = document.getElementById
+    element("damageEntry").val
+    if (element("damageEntry").value && element("fireRateEntry").value && element("magazineSizeEntry").value && element("reloadSpeedEntry").value && element("healthEntry").value) {
+        console.log("Updated!")
+        // timeToKill(element("damageEntry", "fireRateEntry", "magazineSizeEntry", "reloadSpeedEntry", "healthEntry"))
+    }
+}
