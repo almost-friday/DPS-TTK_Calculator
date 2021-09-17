5c064205e45b994e95539d334012c290c260d121
almost_friday <tropicalfruitcake@gmail.com>
2021-09-16 23:01:29 -0700

    woo hooooo everything seems to work
    Co-authored-by: Haggets <Haggets@users.noreply.github.com>

--- Calculator.js
+++ Calculator.js
@@ -1,13 +1,47 @@
 // Calculation
-function timeToKill(damage, fireRate, magazineSize, reloadSpeed, health) {
+function TimeToKill(damage, fireRate, magazineSize, reloadSpeed, health) {
+    let ttk = 0;
+    let currentMagCapacity = magazineSize;
+    console.log("Hello");
+    console.log(currentMagCapacity);
+    currentMagCapacity = FireShot(currentMagCapacity);
+    console.log(currentMagCapacity);
+    /*
+    let timer = 0;
+    if (damage <= 0) {
+        ttk = 0;
+        return;
+    }
+    if (fireRate <= 0) fireRate = 0;
+    ttk = damage * fireRate;
+    console.log(ttk);
+    */
+}
+function FireShot(mag){
+    mag => mag > 0 ? --mag : false;
+    /*
+    if (mag > 0){
+        return --mag;
+    } 
+    else return false;
+    */
+}
 
+function Reload(currentmagcapacity, magcapacity, reloadspeed, totaltime){
+    currentmagcapacity = magcapacity;
+    totaltime += reloadspeed;
 }
 
 // Updates calculation
-function update() {
-    const element = document.getElementById;
-    if (element("damageEntry").value && element("fireRateEntry").value && element("magazineSizeEntry").value && element("reloadSpeedEntry").value && element("healthEntry").value) {
+function Update() {
+    console.log("Updated kindof");
+    let damage = document.getElementById("damageEntry");
+    let fireRate = document.getElementById("fireRateEntry");
+    let magazineSize = document.getElementById("magazineSizeEntry");
+    let reloadSpeed = document.getElementById("reloadSpeedEntry");
+    let enemyHealth = document.getElementById("healthEntry");
+    if (damage.value && fireRate.value && magazineSize.value && reloadSpeed.value && enemyHealth.value) {
         console.log("Updated!");
-        // timeToKill(element("damageEntry"), element("fireRateEntry"), element("magazineSizeEntry"), element("reloadSpeedEntry"), element("healthEntry"));
+        TimeToKill(damage.value, fireRate.value, magazineSize.value, reloadSpeed.value, enemyHealth.value);
     }
 }
