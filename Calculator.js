588deaa19425107977ce6187384713de911ccf1b
almost_friday <tropicalfruitcake@gmail.com>
2021-09-26 19:12:53 -0700

    Debugged the TTK algorithm. Added some documentation to Calculator.js.

--- Calculator.js
+++ Calculator.js
@@ -1,10 +1,10 @@
 //Data Object
 const information = {
     timeToKill: null,
-    damagePerSecond: null,
-    totalTime: 0
+    damagePerSecond: null
 };
 
+// Populates information with several important properties directly from the document.
 for (const field of document.getElementsByClassName("timeToKillInput")) {
     Object.defineProperty(
         information,
@@ -17,7 +17,6 @@ for (const field of document.getElementsByClassName("timeToKillInput")) {
                 value = parseFloat(value || 0);
                 
                 if (value > 0) {
-                    console.log("test");
                     field.value = value;
                 }
                 else {
@@ -30,51 +29,43 @@ for (const field of document.getElementsByClassName("timeToKillInput")) {
     );
 }
 
-information.currentMagazineSize = information.magazineSize;
+// \/ THIS DOESN'T WORK!! it gets assigned before anything is loaded
+/*  // declaring new property in information and assigning to it
+information.currentMagazineSize = Math.ceil(information.magazineSize); */
 
 console.log("Initialized");
 
-    /*
-function information(damage, fireRate, magazineSize, reloadSpeed, enemyHealth) {
-    this.damage = damage.value;
-    this.fireRate = fireRate.value;
-    this.magazineSize = magazineSize.value;
-    this.reloadSpeed = reloadSpeed.value;
-    this.enemyHealth = enemyHealth.value;
-    
-    this.currentMagazineSize = this.magazineSize;
-    this.timeToKill = null;
-    this.damagePerSecond = null;
-    this.totalTime = 0;
-    console.log("Initialized");
-    }
-    */
-
 // Main calculation
 function TimeToKill(stats) {
+    let totalTime = 0;
+    stats.currentMagazineSize = stats.magazineSize;
 
+    //Checking for common errors
+    if (stats.magazineSize < 1) return -1;
+    if (stats.damage < 1) return -2;
+
+    damagePerSecond = stats.damage * stats.fireRate;
 
     while (true) {
-        //stats.currentMagazineSize--;
-        console.log('Ammo in current mag: ' + stats.currentMagazineSize);
         stats.currentMagazineSize -= 1;
         console.log('Ammo in current mag: ' + stats.currentMagazineSize);
 
         stats.enemyHealth -= stats.damage;
-        console.log("Bang!");
         if (stats.enemyHealth <= 0) break;
         
-        stats.totalTime += stats.fireRate.value;
-        if (stats.currentMagazineSize <= 0) {
-            stats.currentMagazineSize = stats.magazineSize;
-            stats.totalTime += stats.reloadSpeed;
+        totalTime += stats.fireRate;
+
+        //Reload if magazine depleted
+        if (stats.currentMagazineSize <= 0) {            
             console.log("Reloading!");
+            stats.currentMagazineSize = stats.magazineSize;
+            totalTime += stats.reloadSpeed;
         } 
     }
-    console.log(stats.currentMagazineSize);
-    console.log(stats.totalTime);
+    console.log('Total time elapsed: ' + totalTime);
 
-    return stats.totalTime;
+    stats.timeToKill = totalTime;
+    return 0;
 }
 
 
@@ -91,8 +82,24 @@ function Update() {
     information.enemyHealth = enemyHealth.value;
 
     if (information.damage && information.fireRate && information.magazineSize && information.reloadSpeed && information.enemyHealth) {
-        console.log("Updated!");
+
         totalTime = TimeToKill(information);
-        timeToKill.value = totalTime;
+
+        switch (totalTime) {
+            case 0:
+                timeToKill.value = information.timeToKill + ' seconds';
+                break;
+            case -1:
+                timeToKill.value = 'The magazine size must be greater than 0!';
+                break;
+            case -2:
+                timeToKill.value = 'The damage must be greater than 0!';
+                break;
+        
+            default:
+                timeToKill.value = null;
+                console.log('Error code does not exist!!');
+                break;
+        }
     }
 }
